b31ceaa541192089c9b458c7695d55b34aa848b8
zarinfam.s <zarinfam.s@gmail.com>
2015-08-09 16:34:05 +0430

    configure play 2.3 with jpa 2.1 and Spring IoC Container 4.0 so that use spring for datasource management.

--- app/configs/DataConfig.java
+++ app/configs/DataConfig.java
@@ -2,13 +2,21 @@ package configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import play.db.jpa.JPA;
 
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.sql.DataSource;
+
+import play.Logger;
+import play.Play;
+
+import java.util.HashMap;
 
 @Configuration
 @EnableTransactionManagement
@@ -16,7 +24,18 @@ public class DataConfig {
 
     @Bean
     public EntityManagerFactory entityManagerFactory() {
-        return Persistence.createEntityManagerFactory("defaultPersistenceUnit");
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(true);
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setPackagesToScan("models");
+        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setJpaPropertyMap(new HashMap<String, String>(){{
+            put("hibernate.hbm2ddl.auto", "create-drop");
+        }});
+        entityManagerFactory.afterPropertiesSet();
+        return entityManagerFactory.getObject();
     }
 
     @Bean
@@ -24,4 +43,13 @@ public class DataConfig {
         return new JpaTransactionManager(entityManagerFactory());
     }
 
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Play.application().configuration().getString("db.default.driver"));
+        dataSource.setUrl(Play.application().configuration().getString("db.default.url"));
+        dataSource.setUsername(Play.application().configuration().getString("db.default.user"));
+        dataSource.setPassword(Play.application().configuration().getString("db.default.password"));
+        return dataSource;
+    }
 }
